8e9d9c4385ccd629ba079edf98cf237cf59effb8
Lenddy Morales <leme2003@gmail.com>
2022-06-13 02:10:59 -0400

    ckeakerboard

--- server.py
+++ server.py
@@ -1,50 +1,15 @@
-from flask import Flask
-
+from flask import Flask,render_template
 app = Flask(__name__)
 
+@app.route("/checkerboard")
+def checkerboard():
+    return  render_template("index.html",first_name = "Lenddy") 
 
-@app.route("/")
-def hello_world():
-    return "Hello world"
-
-
-@app.route("/dojo")
-def dojo():
-    return "dojo"
+@app.route("/checkerboard/2")
+def checkerboard2():
+    return  render_template("index.html",first_name = "Lenddy") 
 
 
-@app.route("/say/flask")
-def say_flask():
-    return "Hi Flask"
 
-
-@app.route("/say/michael")
-def say_michael():
-    return "Hi Michael"
-
-
-@app.route("/say/john")
-def say_John():
-    return "Hi John"
-
-
-@app.route("/repeat/hello/<int:number>")
-def repeat_hello(number):
-    print(number)
-    return " Hello " * number
-
-
-@app.route("/repeat/bye/<int:number>")
-def repeat_bye(number):
-    print(number)
-    return " bye " * number
-
-
-@app.route("/repeat/dogs/<int:number>")
-def repeat_dogs(number):
-    print(number)
-    return " dogs " * number
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
+if  __name__ == "__main__":
+    app.run(debug= True)
