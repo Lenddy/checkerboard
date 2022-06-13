8df1d7da0cacff9e811c1721dad684c56edcf171
Lenddy Morales <leme2003@gmail.com>
2022-06-13 02:13:01 -0400

    cheackerBoard

--- server.py
+++ server.py
@@ -5,11 +5,6 @@ app = Flask(__name__)
 def checkerboard():
     return  render_template("index.html",first_name = "Lenddy") 
 
-@app.route("/checkerboard/2")
-def checkerboard2():
-    return  render_template("index.html",first_name = "Lenddy") 
-
-
 
 if  __name__ == "__main__":
     app.run(debug= True)
